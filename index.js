d916db1133e4583d47dedc8b740370f4bbd0eefb
Mikko Rantanen <jubjub@jubjubnest.net>
2013-05-17 01:00:29 +0300

    Added .jshintrc and style fixes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
+"use strict";
+
 var nodertc = require('bindings')({
     bindings: 'peerconnection.node',
     try: [['module_root', 'compiled', 'platform', 'arch', 'bindings' ]]
 });
 var events = require('events');
 
-// exports.Server = inherits( nodertc.Server, events.EventEmitter );
-exports.PeerConnection = inherits( nodertc.PeerConnection, events.EventEmitter );
-
 function inherits( target, source ) {
-	for( var k in source.prototype )
+	for( var k in source.prototype ) {
 		target.prototype[k] = source.prototype[k];
+    }
 	return target;
 }
+
+exports.PeerConnection = inherits( nodertc.PeerConnection, events.EventEmitter );
+
 
